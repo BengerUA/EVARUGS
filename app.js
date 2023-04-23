a26a243017bbe1ed673a3c82fd33164310c2d1df
BengerMontana <romankolovskiy@gmail.com>
2023-04-23 12:09:11 +0300

    adaptive/mobile first

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    centerMode: true,
+    slidesToShow: 1,
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    centerPadding: '60px',
+  });
+});
+
